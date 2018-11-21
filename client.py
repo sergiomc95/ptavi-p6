0ecb4aff60ad5daa2a0f87644c63c3564a6e34bc
sergiomc95 <sergiomartinc95@gmail.com>
2018-11-21 17:49:53 +0100

    control de argumentos client.py

--- client.py
+++ client.py
@@ -3,25 +3,29 @@
 """
 Programa cliente que abre un socket a un servidor
 """
-
+import sys
 import socket
 
 # Cliente UDP simple.
 
 # Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+if len(sys.argv) != 3:
+    sys.exit("Usage: python client.py method receiver@IP:SIPport")
 
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+METODO = sys.argv[1]
+NOMBRE = sys.argv[2].split('@')[0]
+IP = sys.argv[2].split('@')[1].split(':')[0]
+PORT = int(sys.argv[2].split(':')[1])
+
+LINE = METODO + ' sip:' + NOMBRE + '@' + IP + ':' + str(PORT) + ' SIP/2.0'
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    my_socket.connect((SERVER, PORT))
+    my_socket.connect((IP, PORT))
 
     print("Enviando: " + LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n\r\n')
     data = my_socket.recv(1024)
 
     print('Recibido -- ', data.decode('utf-8'))

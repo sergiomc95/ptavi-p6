4ac8ee09867696777421001954ba35d2de9116a4
sergiomc95 <sergiomartinc95@gmail.com>
2018-11-23 19:43:16 +0100

    avanzo en server.py

--- server.py
+++ server.py
@@ -22,16 +22,34 @@ class EchoHandler(socketserver.DatagramRequestHandler):
     """
 
     def handle(self):
+
         # Escribe dirección y puerto del cliente (de tupla client_address)
         self.wfile.write(b"Hemos recibido tu peticion")
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
+            METODOS = ['INVITE', 'ACK', 'BYE']
             line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
+            linea = line.decode('utf-8')
+            METODO = linea.split(' ')[0]
+            print("El cliente nos manda " + linea)
 
-            # Si no hay más líneas salimos del bucle infinito
+                            # Si no hay más líneas salimos del bucle infinito
             if not line:
                 break
+            if METODO == 'INVITE':
+                print("El cliente nos manda" + line.decode('utf-8'))
+                self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n" + b"SIP/2.0 180 Ring\r\n\r\n" + b"SIP/2.0 200 OK\r\n\r\n" )
+
+            elif METODO == 'ACK':
+                print("El cliente nos manda" + line.decode('utf-8'))
+
+            elif METODO == 'BYE':
+                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+            else:
+                self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
+        else:
+            self.wfile.write(b"SIP/2.0 400 Bad Request")
+    # Si no hay más líneas salimos del bucle infinito
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
